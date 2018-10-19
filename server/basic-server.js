a2ba220c8919b8cda002fa4cd3eda3f635b62b5a
Monkey Do <monkeydo@Monkeys-MacBook-Pro.local>
2018-10-18 18:13:44 -0700

    refactoring

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,6 +1,8 @@
 /* Import node's http module: */
 var http = require('http');
-
+var handleRequest = require('./request-handler');
+var urlParser = require('url');
+var utils = require('./utils');
 
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
@@ -14,17 +16,34 @@ var port = 3000;
 // special address that always refers to localhost.
 var ip = '127.0.0.1';
 
-
-
 // We use node's http module to create a server.
 //
 // The function we pass to http.createServer will be used to handle all
 // incoming requests.
 //
+
+var routes = {
+  '/classes/chatterbox': handleRequest
+};
 // After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
+var server = http.createServer(function(request, response) {
+  //console.log('Serving request type ' + request.method + ' for url ' + request.url);
+
+  var parts = urlParser.parse(request.url);
+
+  var route = routes[parts.pathname];
+  if (route) {
+    route(request, response);
+  } else {
+    utils.sendResponse(response, 'Not Found', 404);
+  }
+
+//  handleRequest(request, response);
+});
 console.log('Listening on http://' + ip + ':' + port);
 server.listen(port, ip);
+
+
 
 // To start this server, run:
 //
